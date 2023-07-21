100dbf07e68c146ac726d72db5a1805e0d7e7339
luan <luaanscar@gmail.com>
2023-07-21 00:52:34 -0300

    Editing animations menu bar

--- resources/views/components/menu-bar/partials/profile-info.blade.php
+++ resources/views/components/menu-bar/partials/profile-info.blade.php
@@ -1,31 +1,36 @@
-<div class="bg-white backdrop-opacity-10 w-full fixed z-0 left-0 h-full top-0">
-    <div class="bg-black absolute w-3/4 absolute left-0 h-full top-0 border-r-1 border-white">
+<div class="max-sm:bg-[#5b708366] w-full absolute   left-0 h-full top-0 z-1 bg-transparent  sm:ease-in duration-100 hidden">
+    <div class="z-0 bg-black absolute w-3/4 absolute left-0 h-full top-0 border-solid border-l-1 border-gray-500 shadow-lg shadow-white/50  ease-in-out duration-300 sm:-translate-x-full">
 
-        <div class="flex flex-row items-center justify-between px-4 ">
+        <div class="flex flex-row font-bold text-lg items-center justify-between pl-4 pr-4 h-[53px] ">
             <span>Informações da conta</span>
-            <x-icons.close class="h-6 w-6"/>
+            <x-icons.close class="h-[25px] w-[25px] mr-[4px]"/>
         </div>
 
-        <div class="flex flex-row items-center justify-between p-4">
-            <div class="rounded-full  bg-slate-500 h-10 w-10"></div>
-            <x-icons.plus class="fill-white border rounded-full border-gray-500 w-8 p-1"/>
-        </div>
+        <div class="flex flex-col p-4">
+            <div class="flex flex-row items-center justify-between">
+                <div class="rounded-full  bg-slate-500 h-10 w-10"></div>
+                <x-icons.plus class="fill-white border rounded-full border-gray-500 w-8 p-1"/>
+            </div>
 
-        <div class="px-4 flex flex-col">
-            <span class="">Name</span>
-            <span class="text-gray-500">@user</span>
+            <div class=" mt-2 flex flex-col">
+                <span class="">Name</span>
+                <span class=" text-gray-500">@user</span>
 
-            <div class="pt-2 text-sm">
-                <strong >354</strong> <span class="text-gray-500">Seguindo</span>
-                <strong >12.4k</strong> <span class="text-gray-500">Seguidores</span>
+                <div class="mt-3 flex justify-between">
+                    <strong>354 <span class="text-gray-500">Seguindo</span>
+                    </strong>
+
+                    <strong>12.4k
+                        <span class="text-gray-500">Seguidores</span>
+                    </strong>
+                </div>
             </div>
         </div>
 
 
-            <x-menu.item class="text-xl mr-4 w-6" icon="profile" title="Perfil"/>
-            <x-menu.item class="text-xl mr-4 w-6" icon="blue" title="Blue"/>
-
-
+        <x-menu.item class="stroke-current w-6 mr-6" icon="profile" title="Perfil"/>
+        <x-menu.item class=" fill-current w-6 mr-6" icon="blue" title="Blue"/>
+        <x-menu.item class=" fill-current w-6 mr-6" icon="lists" title="Listas"/>
 
     </div>
 </div>
--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -1,12 +1,6 @@
-@props(['active', 'route', 'icon', 'title' => null])
+@props(['route', 'icon', 'title' => null])
 
-
-@php
-    $classes = $active ?? false ? 'fill-yell w-8 h-8' : 'stroke-current fill-transparent w-8 h-8';
-    $dot = $active ?? false ? 'absolute bottom-1 block bg-yell rounded-full w-2 h-2' : '';
-@endphp
-
-<div {{$attributes->merge(['class' => $classes])}}>
-    <x-dynamic-component :component="'icons.' . $icon" {{ $attributes->merge(['class' => $classes , 'class' => '']) }} />
-    <div {{ $attributes->merge(['class' => $dot]) }}>{{$title}}</div>
+<div class="flex flex-row p-4 tracking-wide font-medium items-center text-2xl">
+    <x-dynamic-component :component="'icons.' . $icon" {{ $attributes }} />
+    <span>{{$title}}</span>
 </div>
